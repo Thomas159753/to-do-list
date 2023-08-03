f31bd3b59b56eadc307546ac7b06d2899460758e
Thomas159753 <99398525+Thomas159753@users.noreply.github.com>
2023-08-03 23:53:42 +1000

    java html css edits
    project adding and delete work
    added tasks with html and css
    script added to insert tasks

--- src/modules/index.js
+++ src/modules/index.js
@@ -5,21 +5,33 @@ const application = (function() {
     //cache dom
     const $nav = $('.nav-bar');
     const $addProject = $nav.find('#new_project')
+    // cache form dom
     const $form = $nav.find('#project_form');
     const $projectInput = $form.find('#project_input');
     const $btnAddProject = $form.find('.btn_add_project');
     const $btnCancelProject = $form.find('#btn_cancel_project');
+    //cache project dom
+    const $ul = $nav.find('ul');
+
+    const project = new Project();
 
     // bind events
     const bindEvents = () => {
         $btnAddProject.on('click', (event) => addProject())
+        $ul.on('click', 'i.fa-regular', deleteProject);
         $addProject.on('click', hide_show)
     }
 
     const addProject = () => {
-        const project = new Project($projectInput.val());
+        const projectName = $projectInput.val()
+        project.add(projectName)
+        $projectInput.val('')
         hide_show();
     }
+    const deleteProject = (e) => {
+        project.del(e)
+    }
 
-    bindEvents()
+    
+    return bindEvents()
 })()
--- src/modules/projects.js
+++ src/modules/projects.js
@@ -1,27 +1,33 @@
-import hide_show from './hide-show';
 import Mustache from 'mustache';
 
 class Project {
-  constructor(project) {
-    this.project = project;
-    this.cacheDom();
-    this.render();
+  constructor() {
+    this.projects = []
+      this.cacheDom()
   }
 
   cacheDom() {
     this.$nav = $('.nav-bar');
     this.$projectModule = this.$nav.find('#projectModule');
     this.$ul = this.$projectModule.find('ul');
-    // this.$template = this.$projectModule.find('#project-template').html();
+    this.$template = this.$projectModule.find('#project-template').html();
   }
 
   render() {
     const data = {
-      projects: this.project,
+      projects: this.projects,
     };
-    const template = '<li><span>{{projects}}</span><i class="del">X</i></li>'
-    const renderedTemplate = Mustache.render(template, data);
-    this.$ul.append(renderedTemplate);
+    this.$ul.html(Mustache.render(this.$template, data));
+  }
+  add(projectname){
+    this.projects.push({ name: projectname});
+    this.render()
+  }
+  del(e){
+    const $remove = $(e.target).closest('li');
+    const i = this.$ul.find('li').index($remove);
+    this.projects.splice(i, 1);
+    this.render();
   }
 }
 
